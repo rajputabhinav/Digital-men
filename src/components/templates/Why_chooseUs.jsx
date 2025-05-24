bb9e96b48720e42bd77eb05df4bc3259bd40f3be
Abhinav <abhinavrajput37429@gmail.com>
2025-05-24 19:10:20 +0530

    add aos in whychooseUs

--- src/components/templates/Why_chooseUs.jsx
+++ src/components/templates/Why_chooseUs.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css'
 function Why_chooseUs(props) {
+    useEffect(()=>{
+    AOS.init({
+      duration:1200
+    })
+  })
   return (
     <div>
         <h1 className='text-4xl font-bold text-center'>Why Choose Digital Men?</h1>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-18 mt-10 md:items-stretch'>
             <div className='w-[95%] md:w-full m-auto md:m-0 pt-4'>
-                 <img className='w-full aspect-square md:aspect-auto md:h-full rounded-full  md:rounded-r-full md:rounded-l-xl object-cover' src="Images/why-choose-us.jpg"/>
+                 <img className='w-full aspect-square md:aspect-auto md:h-full rounded-full  md:rounded-r-full md:rounded-l-xl object-cover' src="Images/why-choose-us.jpg" data-aos="fade-left"/>
             </div>
-            <div>
+            <div data-aos="fade-up">
             {
                 props.whyChooseUsData.map((data,i)=>(
                 <div className='flex gap-3 items-center mt-4' key={i}>
